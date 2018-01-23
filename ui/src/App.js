d9423f30eebfdcd7d34a18f9808323b4a4974c36
evadb <evademersbrett@gmail.com>
2018-01-23 16:48:33 -0500

    Active link example

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Test from './components/Test'
+import Test2 from './components/Test2'
 import Navigation from './components/Navigation'
+
+import './App.css'
 
 const App = () => (
   <Router>
@@ -10,6 +13,7 @@ const App = () => (
       <Navigation />
       <Route exact path="/" component={Home} />
       <Route path="/test" component={Test} />
+      <Route path="/test2" component={Test2} />
     </div>
   </Router>
 )
--- ui/src/components/Navigation.js
+++ ui/src/components/Navigation.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { withRouter } from 'react-router'
 
 class Navigation extends Component {
   render() {
     return (
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/test">Test</Link>
-      </div>
+      <nav>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+        <NavLink activeClassName="active" to="/test">
+          Test
+        </NavLink>
+        <NavLink activeClassName="active" to="/test2">
+          Test 2
+        </NavLink>
+      </nav>
     )
   }
 }
