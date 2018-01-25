05d57ea41df7cfc7fbbfb5333608964f3bf265e8
evadb <evademersbrett@gmail.com>
2018-01-25 12:49:59 -0500

    Point cypress tests to local host

--- ui/cypress/integration/ui_tests.js
+++ ui/cypress/integration/ui_tests.js
@@ -1,5 +1,11 @@
-describe('My First Test', function() {
-  it('Should pass, we hope', function() {
-    expect(true).to.equal(true)
+describe('NRCAN - example app', function() {
+  describe('Basic Tests', function() {
+    it('Should pass, we hope', function() {
+      expect(true).to.equal(true)
+    })
+
+    it('Should visit the page', function() {
+      cy.visit('/')
+    })
   })
 })
--- ui/src/components/Data.js
+++ ui/src/components/Data.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withApollo } from 'react-apollo'
 
 const Data = () => {
   return (
@@ -8,4 +9,4 @@ const Data = () => {
   )
 }
 
-export default Data
+export default withApollo(Data)
